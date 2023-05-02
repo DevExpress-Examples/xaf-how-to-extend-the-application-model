0dc4a11920c156341bfa0c2107289698267ca3ab
andrey <andrey.kozhevnikov@devexpress.com>
2023-05-02 17:35:58 +0400

    init win

--- CS/ExtendModel.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-<%@ Application Language="C#" CodeBehind="Global.asax.cs" Inherits="ExtendModel.Web.Global"%>
---
+++ CS/XPO/ExtendModel/ExtendModel.Module/DatabaseUpdate/Updater.cs
@@ -0,0 +1,44 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.Data.Filtering;
+using DevExpress.Persistent.Base;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.Xpo;
+using DevExpress.ExpressApp.Xpo;
+using DevExpress.Persistent.BaseImpl;
+using dxTestSolution.Module.BusinessObjects;
+
+namespace ExtendModel.Module.DatabaseUpdate;
+
+// For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.Updating.ModuleUpdater
+public class Updater : ModuleUpdater {
+    public Updater(IObjectSpace objectSpace, Version currentDBVersion) :
+        base(objectSpace, currentDBVersion) {
+    }
+    public override void UpdateDatabaseAfterUpdateSchema() {
+        base.UpdateDatabaseAfterUpdateSchema();
+        //string name = "MyName";
+        //DomainObject1 theObject = ObjectSpace.FirstOrDefault<DomainObject1>(u => u.Name == name);
+        //if(theObject == null) {
+        //    theObject = ObjectSpace.CreateObject<DomainObject1>();
+        //    theObject.Name = name;
+        //}
+        var cnt = ObjectSpace.GetObjects<Contact>().Count;
+        if (cnt > 0) {
+            return;
+        }
+        for (int i = 0; i < 5; i++) {
+            var contact = ObjectSpace.CreateObject<Contact>();
+            contact.FirstName = "FirstName" + i;
+            contact.LastName = "LastName" + i;
+            contact.Age = i * 10;
+            contact.Type = i % 2 == 0;
+        }
+        ObjectSpace.CommitChanges(); //Uncomment this line to persist created object(s).
+    }
+    public override void UpdateDatabaseBeforeUpdateSchema() {
+        base.UpdateDatabaseBeforeUpdateSchema();
+        //if(CurrentDBVersion < new Version("1.1.0.0") && CurrentDBVersion > new Version("0.0.0.0")) {
+        //    RenameColumn("DomainObject1Table", "OldColumnName", "NewColumnName");
+        //}
+    }
+}
