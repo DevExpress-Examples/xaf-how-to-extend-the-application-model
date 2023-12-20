4b549110848225441f4636f5770f009279f19e8f
andrey <andrey.kozhevnikov@devexpress.com>
2023-12-20 19:33:47 +0400

    update controller

--- CS/EFCore/ExtendModelEF/ExtendModelEF.Blazor.Server/Controllers/BlazorGroupFooterViewController.cs
+++ CS/EFCore/ExtendModelEF/ExtendModelEF.Blazor.Server/Controllers/BlazorGroupFooterViewController.cs
@@ -1,31 +1,24 @@
-﻿using DevExpress.ExpressApp;
+﻿using DevExpress.Data;
+using DevExpress.ExpressApp;
 using DevExpress.ExpressApp.Blazor.Editors;
 using DevExpress.ExpressApp.Model;
 
-namespace ExtendModel.Module.Win.Controllers {
-    public class BlazorGroupFooterViewController : ViewController<ListView> {
-
-        protected override void OnViewControlsCreated() {
-            base.OnViewControlsCreated();
-            IModelListViewExtender modelListView = View.Model as IModelListViewExtender;
-            if (modelListView != null && modelListView.IsGroupFooterVisible) {
-                DxGridListEditor gridListEditor = View.Editor as DxGridListEditor;
-                if (gridListEditor != null) {
-                    var dataGridAdapter = gridListEditor.GetGridAdapter();
-                    dataGridAdapter.GridModel.GroupFooterDisplayMode = DevExpress.Blazor.GridGroupFooterDisplayMode.Always;
-                    foreach (IModelColumn modelColumn in View.Model.Columns) {
-                        IModelColumnExtender modelColumnExtender = modelColumn as IModelColumnExtender;
-                        if (modelColumnExtender != null &&
-                            modelColumnExtender.GroupFooterSummaryType != DevExpress.Data.SummaryItemType.None) {
-                            IDxGridSummaryItemsOwner summaryItemsOwner = (IDxGridSummaryItemsOwner)dataGridAdapter;
-                            var summaryItem = (DxGridSummaryItemWrapper)summaryItemsOwner.CreateItem(modelColumn.Id, modelColumnExtender.GroupFooterSummaryType);
-                            summaryItem.SummaryItemModel.FooterColumnName = modelColumn.Id;
-                            summaryItemsOwner.GroupSummary.Add(summaryItem);
-                        }
+namespace ExtendModel.Module.Win.Controllers;
+public class BlazorGroupFooterViewController : ViewController<ListView> {
+    protected override void OnViewControlsCreated() {
+        base.OnViewControlsCreated();
+        if (View.Model is IModelListViewExtender modelListView && modelListView.IsGroupFooterVisible && View.Editor is DxGridListEditor gridListEditor) {
+            gridListEditor.GridModel.GroupFooterDisplayMode = DevExpress.Blazor.GridGroupFooterDisplayMode.Always;
+            if (gridListEditor.GetGridAdapter() is IDxGridSummaryItemsOwner summaryItemsOwner) {
+                foreach (var modelColumn in View.Model.Columns) {
+                    if (modelColumn is IModelColumnExtender modelColumnExtender && modelColumnExtender.GroupFooterSummaryType != SummaryItemType.None) {
+                        var summaryItem = (DxGridSummaryItemWrapper)summaryItemsOwner.CreateItem(modelColumn.Id, modelColumnExtender.GroupFooterSummaryType);
+                        summaryItem.SummaryItemModel.FooterColumnName = modelColumn.Id;
+                        summaryItemsOwner.GroupSummary.Add(summaryItem);
                     }
-
                 }
             }
         }
     }
 }
+
